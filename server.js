c48c99256c63d8d26a36a5161588fd4d3d76a173
Billy Beck <billys.beck@gmail.com>
2025-11-07 10:18:55 -0500

    initial push

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
